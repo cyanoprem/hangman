a23a2b55a740fcd5dbf048237744495cf74bf53e
Cyano Prem <cyano.prem@gmail.com>
2022-09-15 17:10:05 +0530

    Complete project

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,16 +1,70 @@
 # rubocop : disable all
 
+require 'yaml'
+
 class Game
+  attr_accessor :game_name, :secret_word, :dash_string, :guesses_left, :incorrect_guesses
   
-  def initialize()
-    @secret_word = generate_secret_word
-    @dash_string = generate_dash_string
-    @guesses_left = 6
-    @incorrect_guesses = []
-    puts @secret_word
+  def initialize(game_name)
+    @game_name = game_name
+    filename = "saved_games/#{game_name}.yaml"
+    if File.exists?(filename) == false
+      @secret_word = generate_secret_word
+      @dash_string = generate_dash_string
+      @guesses_left = 6
+      @incorrect_guesses = []
+    else
+      @secret_word = saved_secret_word(filename)
+      @dash_string = saved_dash_string(filename)
+      @guesses_left = saved_guesses_left(filename)
+      @incorrect_guesses = saved_incorrect_guesses(filename)
+    end    
+    # p @game_name
+    # p @secret_word
     puts string_with_spaces(@dash_string) 
   end
-  
+
+  def save_game
+    Dir.mkdir('saved_games') unless Dir.exist?('saved_games')
+
+    filename = "saved_games/#{@game_name}.yaml"
+
+    File.open(filename, 'w') do |file|
+      file.puts YAML.dump ({
+        :game_name => @game_name,
+        :secret_word => @secret_word,
+        :dash_string => @dash_string,
+        :guesses_left => @guesses_left,
+        :incorrect_guesses => @incorrect_guesses
+    })
+    end
+  end
+
+  def self.get_saved_game(file)
+    data = YAML.load_file(file)
+    self.new(data[:game_name])
+  end
+
+  def saved_secret_word(file)
+    data = YAML.load_file(file)
+    data[:secret_word]
+  end
+
+  def saved_dash_string(file)
+    data = YAML.load_file(file)
+    data[:dash_string]
+  end
+
+  def saved_guesses_left(file)
+    data = YAML.load_file(file)
+    data[:guesses_left]
+  end
+
+  def saved_incorrect_guesses(file)
+    data = YAML.load_file(file)
+    data[:incorrect_guesses]
+  end
+
   def generate_secret_word
     file = File.open("google-10000-english-no-swears.txt")
     words_array = file.readlines.map(&:chomp)
@@ -34,12 +88,28 @@ class Game
         puts "Oops! Number of guesses are over. The correct answer is #{@secret_word}"
         return
       else
-        puts "You have #{@guesses_left} incorrect guesses left"
-        puts "Incorrect guesses: #{@incorrect_guesses}" 
-        print 'Guess the letter: '
-        guessed_letter = gets.chomp.downcase
-        change_dash_string(guessed_letter)
+        save_or_play
+        if $save_or_play == 2
+          return
+        end
       end
+    end
+  end
+
+  def save_or_play
+    puts "You have #{@guesses_left} incorrect guesses left"
+    puts "Incorrect guesses: #{@incorrect_guesses}" 
+    print 'Enter 1 to Guess the letter or Enter 2 to Save the Game: '
+    $save_or_play = gets.chomp.to_i
+    if $save_or_play == 2
+      save_game
+    elsif $save_or_play == 1
+      print 'Guess the letter: '
+      guessed_letter = gets.chomp.downcase
+      change_dash_string(guessed_letter)
+    else
+      puts 'Invalid Input!!!'
+      save_or_play
     end
   end
 
@@ -64,4 +134,15 @@ class Game
 
 end
 
-Game.new().play
+print "Give a name to your Game or enter the name of your Saved Game: "
+game_name = gets.chomp
+
+if File.exists?("saved_games/#{game_name}.yaml")
+  game = Game.get_saved_game("saved_games/#{game_name}.yaml")
+else
+  game = Game.new(game_name)
+end
+
+game.play()
+
+
